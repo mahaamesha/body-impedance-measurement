87ab5c409a7409789241ecf3babfadc41bcc7f62
mahaamesha <mahaamesha@gmail.com>
2022-08-19 09:15:14 +0700

    breakpoint

--- src/send_serial_data.py
+++ src/send_serial_data.py
@@ -9,34 +9,40 @@ baudrate = 9600
 ser = Serial(port, baudrate)
 
 
-def send_serial_data(data_str, debug_flag=False):
+def send_serial_data(data, debug_flag=False, max_chr=16):
+    data_str = str(data)[:max_chr]       # extract only first n character
     data_byte = bytes(data_str, "utf-8")
     # send every character to arduino from serials
     ser.write(data_byte)
+    print(data_str, data_byte)
 
     if debug_flag:
         for i in range(len(data_byte)):
             # read every character in data_byte. for debugging purpose
-            print(ser.readline()[:-2])  # format: b'49\r\n' --> b'49'
+            incoming_byte = ser.readline()[:-2]     # format: b'49\r\n' --> b'49'
+            print("%s\t: %s" %(i, incoming_byte))
 
 
 # i need to send data to arduino via serial: internal factor & model coefficient
 def send_serial_internal_factor(file_path="tmp/training_internal_factor.json"):
     data = read_filejson(file_path)
     keys = list( data.keys() )
+    
 
-    for key in keys:
-        arr = data[key]     # get the array of delta_z & delta_phase
-        for i in range(len(arr)):   # iterate in each element of that array
-            num = arr[i]
-            num_str = "{:.4f}".format(num)
-            send_serial_data(num_str)
-        break
+    for key in keys:    # delta_z & delta_phase
+        arr_num = data[key]     # get the array of delta_z & delta_phase
+        for i in range(len(arr_num)):   # iterate in each element of that array
+            num = arr_num[i]      # num is float type
+
+            # send every num through serial communication
+            send_serial_data(num, debug_flag=True, max_chr=8)
+        
+        print(len(arr_num))
 
 
 
 if __name__ == "__main__":
-    test_str = "1007.81"
-    data_byte = send_serial_data(test_str, debug_flag=True)
+    test_str = 2.1
+    send_serial_data(test_str, debug_flag=True)
 
-    send_serial_internal_factor()
+    # send_serial_internal_factor()
